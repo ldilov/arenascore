39be17d5a9ba151c8aa675a36a2559afed1c90d9
Lazar Dilov <ldilov@isimarkets.com>
2024-08-24 19:42:23 +0300

    TWW

--- src/Constants/PatchVariables.lua
+++ src/Constants/PatchVariables.lua
@@ -2,7 +2,7 @@
 
 -- Update current version below
 PatchVariables = {
-    currentVersion = 100207,
+    currentVersion = 110002,
     cacheTimeOut = 300
 }
 
@@ -17,7 +17,23 @@ local retail_100207 = {
     version = 100207
 }
 
+local retail_110002 = {
+    ratingWeight = 0.8,
+    winLossWeight = 0.05,
+    itemLevelWeight = 0.15,
+    maxItemLevel = 636,
+    maxMetric = 6000,
+    minMetric = 0,
+    maxRating = 3000,
+    version = 110002
+}
+
+
 -- Add patch variables below
 PatchVariables[100207] =  {
     profile = retail_100207
+}
+
+PatchVariables[110002] =  {
+    profile = retail_110002
 }
